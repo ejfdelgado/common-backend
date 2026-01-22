6477f4ba9ea30f5c5a9640d708b012189182d824
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-01-22 15:29:06 -0500

    ok

--- src/services/firestoreWeb.ts
+++ src/services/firestoreWeb.ts
@@ -2,6 +2,7 @@ import { ApiResponse, AuthenticatedRequest } from "../types";
 import { Response } from 'express';
 import { General } from "../tools/General";
 import { MyStore } from "./firestore";
+import { MyUtilities } from 'ejfdelgado-common-ts';
 
 export class FirestoreWeb {
     static async createUpdate(req: AuthenticatedRequest, res: Response) {
@@ -13,7 +14,12 @@ export class FirestoreWeb {
         const now = Date.now();
         const collection = General.readParam(req, "collection", undefined, true);
         const data: any = General.readParam(req, "data", undefined, true);
-        const conf: any = General.readParam(req, "conf", undefined, true);
+        let confIn: any = General.readParam(req, "conf", {}, false);
+
+        const conf = Object.assign({
+            autoAuthor: true,
+            searchFields: [],
+        }, confIn);
 
         delete data.created;
         delete data.author;
@@ -38,18 +44,47 @@ export class FirestoreWeb {
 
         };
 
+        function filterObject(dataIn: any, searchFields: string[]): { [key: string]: string } {
+            const searchValues: { [key: string]: string } = {};
+            Object.keys(dataIn)
+                .filter(el => searchFields.indexOf(el) >= 0)
+                .forEach(key => {
+                    const val = dataIn[key];
+                    if (typeof val == "string") {
+                        searchValues[key] = val;
+                    } else if (val instanceof Array) {
+                        searchValues[key] = val.join(" ");
+                    }
+                });
+            return searchValues;
+        }
+
+        function computeSearchable(data: { [key: string]: string }): string[] {
+            const keys = Object.keys(data);
+            const completeText = keys.map(key => data[key]).join(" ");
+            return MyUtilities.partirTexto(completeText);
+        }
+
+        let actualSearchables = filterObject(data, conf.searchFields);
+
         if (typeof id == "string") {
             // Maybe a creation
             const exists = await MyStore.exist(collection, id);
             if (!exists) {
                 autoCreation();
             } else {
+                // Exists before, read parameters to compute searchable
+                const oldDoc = await MyStore.readById(collection, id);
+                const oldSearchables = filterObject(oldDoc, conf.searchFields);
+                actualSearchables = Object.assign(oldSearchables, actualSearchables);
                 data.updated = now;
             }
+            data.search = computeSearchable(actualSearchables);
             dbResponse = await MyStore.updateOrCreateById(collection, id, data);
         } else {
             // Sure a creation
             autoCreation();
+            data.search = computeSearchable(actualSearchables);
             dbResponse = await MyStore.create(collection, data);
             id = dbResponse.id;
         }
