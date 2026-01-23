50a28e33eb6a09796425e341e4d4be4208ee67d5
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-01-23 09:03:02 -0500

    ok

--- src/services/templates.ts
+++ src/services/templates.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { ApiResponse, AuthenticatedRequest } from '../types';
-import { General } from '../tools/General';
+import { escapeHtml, General, getBucketFilePath, getThumbnailPath } from '../tools/General';
 import { MyStore } from './firestore';
 import { MyTemplate } from 'ejfdelgado-common-ts';
 import fs from 'fs';
@@ -8,29 +8,17 @@ import path from 'path';
 
 const templateEngine = new MyTemplate();
 
-export function getThumbnailPath(value: string) {
-    return value.replace(/\.[a-z\?=\d]+$/ig, (extension: string) => {
-        return "_xs" + extension;
-    });
-}
 
-export function getBucketFilePath(value: string | null) {
-    if (value != null && value.length > 0) {
-        return `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${value}`;
-    } else {
-        return `https://storage.googleapis.com/${process.env.BUCKET_NAME}/social_image.jpg`;
+
+export function truncateString(max: number, val?: string) {
+    if (!val) {
+        return val;
     }
-}
-
-function escapeHtml(str: string) {
-    if (typeof str !== 'string') return '';
-
-    return str
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;');
+    if (val.length > max) {
+        return val.substring(0, max) + "...";
+    } else {
+        return val;
+    }
 }
 
 export class TemplatesSrv {
@@ -58,23 +46,14 @@ export class TemplatesSrv {
 
         const data = {
             photo,
-            title: escapeHtml(title),
-            description: escapeHtml(description),
+            title: truncateString(50, escapeHtml(title)),
+            description: truncateString(120, escapeHtml(description)),
             url,
         };
 
         const filePath = path.join(__dirname, '../assets/html/social.html');
         const content = fs.readFileSync(filePath, 'utf8');
         const rendered = templateEngine.render(content, data);
-
-        /*
-        const response: ApiResponse = {
-            success: true,
-            message: 'Ok',
-            data: data,
-            timestamp: new Date()
-        };
-        */
 
         res.status(200).send(rendered);
     }
--- src/tools/General.ts
+++ src/tools/General.ts
@@ -10,6 +10,42 @@ export function asyncHandler<T extends (req: Request, res: Response, next: NextF
     };
 }
 
+export function getThumbnailPath(value: string) {
+    return value.replace(/\.[a-z\?=\d]+$/ig, (extension: string) => {
+        return "_xs" + extension;
+    });
+}
+
+export function getBucketFilePath(value: string | null) {
+    if (value != null && value.length > 0) {
+        return `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${value}`;
+    } else {
+        return `https://storage.googleapis.com/${process.env.BUCKET_NAME}/social_image.jpg`;
+    }
+}
+
+export function escapeHtml(str: string) {
+    if (typeof str !== 'string') return '';
+
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
+export function truncateString(max: number, val?: string) {
+    if (!val) {
+        return val;
+    }
+    if (val.length > max) {
+        return val.substring(0, max) + "...";
+    } else {
+        return val;
+    }
+}
+
 export class General {
 
     static readParam(req: Request, name: string, pred: any = null, complain: boolean = false) {
