f5dc7a36848f73543d4dd13ba908eae3a2508526
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2026-03-01 08:28:23 -0500

    ok

--- data/supabase/create.sql
+++ data/supabase/create.sql
@@ -2,14 +2,15 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE document_embeddings (
-    id TEXT,
+    id UUID DEFAULT gen_random_uuid(),
     parent TEXT,
     created_at BIGINT NOT NULL DEFAULT extract(epoch from now())::bigint,
     embedding vector(1024),
+    metadata JSONB,
     PRIMARY KEY (parent, id)
 );
 
 -- Create an HNSW index for fast similarity search
 CREATE INDEX ON document_embeddings USING hnsw (embedding vector_cosine_ops);
 
-CREATE INDEX idx_parent ON document_embeddings (parent);
+CREATE INDEX document_embeddings_parent ON document_embeddings (parent);
